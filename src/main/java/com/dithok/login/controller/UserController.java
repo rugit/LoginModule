3c61438b9b7b776367ec8faf27f5bd07ce3976f9
rurugit <neog.anupal101@gmail.com>
2019-09-04 22:32:31 +0530

    Create PUSH and GET route

--- src/main/java/com/dithok/login/controller/UserController.java
+++ src/main/java/com/dithok/login/controller/UserController.java
@@ -13,4 +13,38 @@ public class UserController{
 	{
 		return "<h1>Hello Users!! Page is currently being build. other route '/api/users', '/api/addusers' use postman</h1>";
 	}
+
+	//GET route to return class structure
+	@RequestMapping("/api/users")
+	public Object apiUsers()
+	{
+		return "{userName : xyz , userPassword : xyz, userMobileNumber: xyz, userEmailID: xyz, userAddress: xyz, userGender : xyz}";
+	}
+
+	//GET route
+	@RequestMapping("/api/addusers")
+	public String getData(@RequestParam(value = "userName", defaultValue="Anupal", required=false) String userName,
+						@RequestParam(value = "userPassword", defaultValue="notaprogrammeryet", required=false) String userPassword,
+						@RequestParam(value = "userMobileNumber", defaultValue="0000000000", required=false) String userMobileNumber,
+						@RequestParam(value = "userEmailID", defaultValue="johndoe@gmail.com", required=false) String userEmailID,
+						@RequestParam(value = "userAddress", defaultValue="unknown", required=false) String userAddress,
+						@RequestParam(value = "userGender", defaultValue="male", required=false) String userGender
+						)
+	{
+		
+		UserModel user = new UserModel(userName, userPassword, userMobileNumber, userEmailID, userAddress, userGender);
+
+		return "GET method is called. Username is <b>" + user.getUserName() + "</b>. MobileNumber is <b>" + user.getUserMobileNumber()+"</b>";
+		
+	}
+
+	//POST route
+	@RequestMapping(method = RequestMethod.POST, value = "/api/addusers")
+	public String registerUser(@RequestBody UserModel user){
+
+		return "POST method is called. Username is <b>" + user.getUserName() + "</b>. Password is <b>" + user.getUserPassword()+"</b>";
+	}	
 }
+
+
+	
