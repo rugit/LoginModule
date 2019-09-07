40af77a467371cc374fa6f98e3a463b0a7affe48
rurugit <neog.anupal101@gmail.com>
2019-09-08 02:15:48 +0530

    Add New Controller

--- src/main/java/com/dithok/login/controller/UserController.java
+++ src/main/java/com/dithok/login/controller/UserController.java
@@ -2,48 +2,27 @@ package com.dithok.login.controller;
 
 import com.dithok.login.model.UserModel;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.BeanUtils;
+
 
 @RestController
 @RequestMapping("/")
-public class UserController{
-
-	//Default route 
-	@RequestMapping("/")
-	@GetMapping
-	public String helloWorld()
-	{
-		return "<h1>Hello Users!! Page is currently being build. other route '/api/users', '/api/addusers' use postman</h1>";
-	}
-
-	//GET route to return class structure
-	@RequestMapping("/api/users")
-	public Object apiUsers()
-	{
-		return "{userName : xyz , userPassword : xyz, userMobileNumber: xyz, userEmailID: xyz, userAddress: xyz, userGender : xyz}";
-	}
-
-	//GET route
-	@RequestMapping("/api/addusers")
-	public String getData(@RequestParam(value = "userName", defaultValue="Anupal", required=false) String userName,
-						@RequestParam(value = "userPassword", defaultValue="notaprogrammeryet", required=false) String userPassword,
-						@RequestParam(value = "userMobileNumber", defaultValue="0000000000", required=false) String userMobileNumber,
-						@RequestParam(value = "userEmailID", defaultValue="johndoe@gmail.com", required=false) String userEmailID,
-						@RequestParam(value = "userAddress", defaultValue="unknown", required=false) String userAddress,
-						@RequestParam(value = "userGender", defaultValue="male", required=false) String userGender
-						)
-	{
-		
-		UserModel user = new UserModel(userName, userPassword, userMobileNumber, userEmailID, userAddress, userGender);
-
-		return "GET method is called. Username is <b>" + user.getUserName() + "</b>. MobileNumber is <b>" + user.getUserMobileNumber()+"</b>";
-		
-	}
+public class UserRestController{
 
 	//POST route
-	@RequestMapping(method = RequestMethod.POST, value = "/api/addusers")
-	public String registerUser(@RequestBody UserModel user){
+	@RequestMapping(method = RequestMethod.POST, value = "/api/restaddusers")
+	public String registerUser(@RequestBody UserRequestModel user){
 
-		return "POST method is called. Username is <b>" + user.getUserName() + "</b>. Password is <b>" + user.getUserPassword()+"</b>";
+		UserResponseModel returnValue = new UserResponseModel();
+
+		UserDto userDto = new UserDto();
+		BeanUtils.copyProperties(user, userDto);
+
+		userDto createdUser = UserService.createUser(userDto);
+		BeanUtils.copyProperties(createdUser, returnValue);
+
+		return returnValue;
+		// return "POST method is called. Username is <b>" + user.getUserName() + "</b>. Password is <b>" + user.getUserPassword()+"</b>";
 	}	
 }
 
